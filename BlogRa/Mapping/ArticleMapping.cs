6a547bb18131689b3fcbfccc7a090846b49c5f52
mary <marya.6t@gmail.com@gmail.com>
2024-02-05 12:00:19 -0800

    Created Aricle entity and it's mapping class

---
+++ BlogRa/Mapping/ArticleMapping.cs
@@ -0,0 +1,15 @@
+﻿using BlogRa.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogRa.Mapping
+{
+    public class ArticleMapping : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.ToTable("Articles");
+            builder.HasKey(a => a.Id);
+        }
+    }
+}
